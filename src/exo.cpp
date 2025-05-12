6300552e7d6ae7600d182e21d306ea01c08ca39c
Nick Horton <nickkbhorton@gmail.com>
2025-05-12 02:48:00 +0000

    terminal message system

--- src/exo.cpp
+++ src/exo.cpp
@@ -5,12 +5,15 @@
 #include <lua.h>
 #include <lua5.3/lauxlib.h>
 #include <stdexcept>
+#include <string>
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
 #include <lua.hpp>
 
 int constexpr in_fd{0};
+char const* TERMESC{"\x1b"};
 
 class Terminal
 {
@@ -33,6 +36,10 @@ public:
             int sen = errno;
             throw std::runtime_error("Terminal::Terminal::tcsetattr: " + std::string(strerror(sen)));
         }
+        struct winsize w;
+        ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+        width = w.ws_col;
+        height = w.ws_row;
     }
     ~Terminal()
     {
@@ -45,6 +52,31 @@ public:
     Terminal(Terminal&&) = delete;
     Terminal& operator=(const Terminal&) = delete;
     Terminal& operator=(Terminal&&) = delete;
+    void clear(bool flush = true)
+    {
+        std::cout << TERMESC << "[2J";
+        if (flush)
+            fflush(stdout);
+    }
+    void go_to(int x, int y, bool flush = true)
+    {
+        std::cout << TERMESC << "[" << std::to_string(y) << ";" << std::to_string(x) << "H";
+        if (flush)
+            fflush(stdout);
+    }
+    void write_message(std::string const& msg, bool flush = true)
+    {
+        std::cout << TERMESC << "7";
+        go_to(0, height, false);
+        std::cout << msg;
+        std::cout << TERMESC << "8";
+        if (flush)
+            fflush(stdout);
+    }
+
+public:
+    int width;
+    int height;
 
 private:
     termios m_saved_termios;
@@ -102,6 +134,10 @@ int main()
 {
     Config config{};
     Terminal terminal{};
+    terminal.clear();
+    terminal.go_to(0, 0);
+    terminal.write_message("hello terminal");
+
     char c;
     while (read(in_fd, &c, 1) > 0 && c != config.exit_key) {
         if (iscntrl(c)) {
